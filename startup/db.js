86ee3069c45037dbc35666bd992ec33a24ec3fb6
tohbey <fafoworatobi25@gmail.com>
2022-02-18 17:57:07 +0100

    error fix

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const { sequelize } = require('../models');
 const connectDb =  async() => {
     await sequelize.authenticate().then(() => {
         console.log('connected')
-    }).catch(err => console.loh(err))
+    }).catch(err => console.log(err))
 }
 
 module.exports = connectDb;
